46e097f755199b1b646c3b47c177e04e4d8890f3
Diego Ferreira <katesclau@gmail.com>
2018-04-11 15:37:52 -0300

    Adds base css

--- src/Footer.js
+++ src/Footer.js
@@ -3,21 +3,17 @@ import React, { PureComponent } from 'react';
 /**
  * A Footer with address, contact info and social networks
  */
-class Footer extends PureComponent {
-  render() {
-    return (
-      <div className="aether-footer">
-        <div className="aether-address">
-          {this.props.address}
-        </div>
-        <div className="aether-contact">
-          {this.props.contact}
-        </div>
-        <div className="aether-social">
-        </div>
-      </div>
-    );
-  }
-}
+const Footer = (props) => (
+  <div className="aether-footer">
+    <div className="aether-address">
+      {props.address}
+    </div>
+    <div className="aether-contact">
+      {props.contact}
+    </div>
+    <div className="aether-social">
+    </div>
+  </div>
+);
 
 export default Footer;
--- src/Header.js
+++ src/Header.js
@@ -3,23 +3,18 @@ import React, { PureComponent } from 'react';
 /**
  * A Header with logo and a RouterSwitch menu
  */
-class Header extends PureComponent {
-
-  render() {
-    return (
-      <div className="aether-header">
-        <img className="aether-logo"
-          src={this.props.logo.url}
-          alt={this.props.logo.alt}
-        />
-        <div className="aether-header">
-          <h1 className="aether-header-text">{this.props.text}</h1>
-        </div>
-        <div className="aether-menu">
-        </div>
-      </div>
-    );
-  }
-}
+const Header = (props) => (
+  <div className="aether-header">
+    <img className="aether-logo"
+      src={props.logo.url}
+      alt={props.logo.alt}
+    />
+    <div className="aether-header">
+      <h1 className="aether-header-text">{props.text}</h1>
+    </div>
+    <div className="aether-menu">
+    </div>
+  </div>
+);
 
 export default Header;
